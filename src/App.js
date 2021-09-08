d4a700de81ef1258ec77850d1bb5f302f91444e2
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2021-09-08 11:37:36 +0530

    edit app file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Page from './Blog/Pages/Products/P'
-//import Page from './BrianCode/Footer/Footer'
-//import Bootstrap from './Duplication/Duplication'
-//import Bootstrap from './Rest/Search/Search'
-//import Bootstrap from './Loading/Index'   //very important
-//import Page from './Practice/code15/Practice'
-//import Bootstrap from './Redux2/Practice/index'
-//import Bootstrap from './Store/Example2/index'
-//import Bootstrap from './Bootstrap/Navs'
-//import Bootstrap from './Redux4/index'
-//import Bootstrap from './Codevelution2/useState/InputName2'
-//import Bootstrap from './Practice/Practice/Index'
-//import Bootstrap from './Test/Test'
-//import Bootstrap from './styledComponent/Button4'
 import Bootstrap from './storage/localClass'
 
 function App() {
