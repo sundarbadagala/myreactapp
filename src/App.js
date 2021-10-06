43e1042ce2703f459723c97e1814caaaeaa2ec1d
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2021-10-06 15:42:28 +0530

    import file from submodule

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from './reactcomponents/src/components/Header'
+import Main from './reactcomponents/src/components/Button'
 
 function App() {
   return (
     <div>
       <Main/>
+      Main component
     </div>
   );
 }
