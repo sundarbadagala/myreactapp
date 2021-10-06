ffdde0d8e5ae5ffc1a45ffe3d1af02643c8737ca
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2021-10-06 16:52:16 +0530

    add button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from './reactcomponents/src/components/Button'
+import {Button} from './Bootstrap/mainbutton'
 
 function App() {
   return (
     <div>
-      <Main/>
-      Main component
+      <Button/>
     </div>
   );
 }
