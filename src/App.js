d782514051bc93c3975f55a749faac3318ed4c3b
sundarbadagala <74392647+sundarthedevoloper@users.noreply.github.com>
2021-10-06 15:11:00 +0530

    import sub module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from './Practice/Utils'
+import Main from './reactcomponents/src/App'
 
 function App() {
   return (
